75dbb73a9e5e638a8fbe398cb157e217a47fcf8d
Hanhua Feng <hanhua.feng@askherefirst.com>
2025-08-22 13:11:00 +0000

    Addded a new app called Trikona.

--- fractal/assets/three-BAYWCrVo.js
+++ fractal/assets/three-BAYWCrVo.js
@@ -1,4 +1,4 @@
-import{r as St,c as ds,j as pn,a as xp,R as vp,s as mh,F as yp,u as Mp,b as Sp}from"./other-DeAWH2A7.js";/**
+import{r as St,c as ds,j as pn,a as xp,R as vp,s as mh,F as yp,u as Mp,b as Sp}from"./other-CR04kSd_.js";/**
  * @license
  * Copyright 2010-2025 Three.js Authors
  * SPDX-License-Identifier: MIT
